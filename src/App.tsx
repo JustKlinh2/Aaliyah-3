757c2e54ff2e4b09294c3067629742b3a856f99f
CodeSandbox <support@codesandbox.io>
2024-02-02 16:49:53 +0000

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,64 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
+const phrases = [
+  "No? :c",
+  "why :c say yes bruh",
+  "please please please :c",
+  "pretty honey please be my valentineeee",
+  "About to literally cry",
+  "WAHHHHHHHHHHH",
+  "say yes >:c",
+];
+
 function App() {
-  const [count, setCount] = useState(0);
+  const [noCount, setNoCount] = useState(0);
+  const [yesPressed, setYesPressed] = useState(false);
+  const yesButtonSize = noCount * 20 + 16;
+
+  function handleNoClick() {
+    setNoCount(noCount + 1);
+  }
+
+  function getNoButtonText() {
+    return phrases[Math.min(noCount, phrases.length - 1)];
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="For my honey Aaliyah">
+      {yesPressed ? (
+        <>
+          <img
+            alt="valentine cat"
+            src="https://gifdb.com/images/high/milk-and-mocha-kiss-2vwjr4s7usa2g5kj.gif"
+          />
+          <div className="text">
+            YAYYAYAYAYAY, THANK YOU FOR BEING MY VALENTINE I LOVE YOU SO MUCH!!!
+          </div>
+        </>
+      ) : (
+        <>
+          <img
+            alt="Two bears"
+            src="https://gifdb.com/images/high/milk-and-mocha-bear-498-x-424-gif-86yyedkwh61tdqpt.gif"
+          />
+
+          <div>Will you please be my valentine C:</div>
+          <div>
+            <button
+              className="yesButton"
+              style={{ fontSize: yesButtonSize }}
+              onClick={() => setYesPressed(true)}
+            >
+              Yes
+            </button>
+            <button onClick={handleNoClick} className="noButton">
+              {getNoButtonText()}
+            </button>
+          </div>
+        </>
+      )}
+    </div>
   );
 }
 
